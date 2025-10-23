41ed6a3c84af2a344e4800a2d010a0d3d77fa4cb
Roshita-Guthikonda <roshitaguthikonda28@gmail.com>
2025-10-23 23:35:10 +0530

    Nature Sequence Generator

---
+++ FibonacciSeries.java
@@ -0,0 +1,21 @@
+package com.codegnan.controlstatements;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int num1=0,num2=1;
+		System.out.print(num1+" "+num2);
+		for(int i=2;i<n;i++) {
+			int num3=num1+num2;
+			System.out.print(" "+num3);
+			num1=num2;
+			num2=num3;
+		}
+		sc.close();
+	}
+
+}
